32f439b2d3c4fbb41b9e9b1f489392d877681745
Daniel <danielmj@me.com>
2016-03-10 20:15:28 -0800

    Added simple tests

--- circleci-test.sh
+++ circleci-test.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Perform any cool tests here.. Havent had time yet."
+echo "Waiting..."
+
+sleep 5;
+
+echo "--> Downloading JSON Parser"
+wget http://stedolan.github.io/jq/download/linux64/jq
+chmod +x jq
+
+echo "--> Getting Website Api Status"
+status=$(curl 'http://localhost:80/api/status/' | ./jq -r '.status')
+if [ "$status" == "success" ]; then
+  echo "TEST SUCCESS!"
+else
+  echo "TEST FAILED! $status"; exit 1
+fi
